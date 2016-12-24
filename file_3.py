eaf3b5d92c9f81c6eb65998b11a60e31b8347618
plemb <pirmin.lemberger@gmail.com>
2016-12-24 14:06:41 +0100

    ajout du fichier_3

---
+++ file_3.py
@@ -0,0 +1,9 @@
+from random import choice
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+# an empty python file with some declarations
